872d7ce68dcf9d44991f7c653b39e5c911bd88f6
Nandakumar Nagarajan <41432436+nandakumar-nagarajan@users.noreply.github.com>
2024-04-11 17:19:41 +0530

    Init Github Actions Test

---
+++ Utilities/Utilities/StringLibrary.cs
@@ -0,0 +1,21 @@
+﻿namespace Utilities;
+
+public static class StringLibrary
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+
+    public static int WordCount(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return 0;
+            
+        return str.Trim().Length + 1;
+    }
+}
